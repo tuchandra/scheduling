545b515b2b33b3eab1cc2e948959766b172f621d
Tushar Chandra <TusharChandra2018@u.northwestern.edu>
2016-08-19 10:52:51 -0500

    Add tests for pref string conversion

--- tests/tests.py
+++ tests/tests.py
@@ -6,11 +6,6 @@ import unittest
 class TestPrefReader(unittest.TestCase):
     """ Tests for the functions to read preferences. """
 
-    def test_employee_names(self):
-        """ Test that get_all_prefs() gets the right employee names. """
-
-        assert scheduler.get_all_prefs("tests/test_prefs") == 2
-
     def test_combine_lines(self):
         """ Test that combine_lines() properly combines lines. """
 
@@ -45,3 +40,66 @@ class TestPrefReader(unittest.TestCase):
         combined = scheduler.combine_lines(lines)
         print (combined)
         assert combined == expected_empls
+
+    def test_get_day_prefs_simple(self):
+        """ Test get_day_prefs() on the dummy set of prefs. """
+
+        Employee = namedtuple("Employee", ["name", "prefs"])
+
+        actual_prefs = scheduler.get_day_prefs("tests/test_prefs")
+        expected = [Employee('Some Employee', 'XXXXXXXXXXXXDDDDDDDDPPPPPPCCCCCCCCCCCCCCCCXXXXXXXXXX'),
+                    Employee('Test Student', 'XXXXXXCCCCCCCCCCCCCCCCCCCCXXPPPPPPPPPPPPPPPPXXXXXXXX')]
+
+
+        assert actual_prefs == expected
+
+    def test_its_fine_if_this_fails(self):
+        """ Intentionally failing so I can get printed stuff. """
+
+        assert 1
+        #assert scheduler.get_day_prefs("tests/test_prefs") == 0
+
+class TestPrefsLineConversion(unittest.TestCase):
+    """ Tests for the functions that convert prefs lines to strings. """
+
+    def test_simple_uncolored_conversion(self):
+        """ Test a conversion from one tb(..) piece. """
+        line = 'tb(0,4);'
+        expected = 'XXXX'
+
+        assert scheduler.prefs_line_to_string(line) == expected
+
+    def test_simple_colored_conversion(self):
+        """ Test a conversion for a green tc(..) piece. """
+        line = 'tc(4,8,"1");'
+        expected = 'PPPPPPPP'
+
+        assert scheduler.prefs_line_to_string(line) == expected
+
+    def test_simple_colored_conversion(self):
+        """ Test a conversion for a pink tc(..) piece. """
+        line = 'tc(4,6,"2");'
+        expected = 'DDDDDD'
+
+        assert scheduler.prefs_line_to_string(line) == expected
+
+    def test_simple_colored_conversion(self):
+        """ Test a conversion for a red tc(..) piece. """
+        line = 'tc(4,4,"3");'
+        expected = 'CCCC'
+
+        assert scheduler.prefs_line_to_string(line) == expected
+
+    def test_double_conversion(self):
+        """ Test a conversion with two pieces. """
+        line = 'tc(2,4,"1");tb(0,8);'
+        expected = 'PPPPXXXXXXXX'
+
+        assert scheduler.prefs_line_to_string(line) == expected
+
+    def test_multiple_conversion(self):
+        """ Test a conversion with multiple pieces. """
+        line = 'tb(0,4);tc(0,14,"3");tc(2,2,"1");tc(0,26,"3");tb(2,6);etr();'
+        expected = 'XXXXCCCCCCCCCCCCCCPPCCCCCCCCCCCCCCCCCCCCCCCCCCXXXXXX'
+
+        assert scheduler.prefs_line_to_string(line) == expected
